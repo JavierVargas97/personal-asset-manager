031f74306b33ddd7f630c0ab3ab3b6b8a0ffd780
Pablo Garcia <pgarciag566@gmail.com>
2022-04-23 00:37:29 -0500

    ya jala el search bar de los cryptos, pero tengo que econtrar una manera de filtrarlo

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-//
+//Esto hace que el Modal sirva.
 let modal = document.getElementById("mymodal")
 let assetbtn = document.getElementById("assetbtn")
 let span = document.getElementsByClassName("close");
@@ -18,24 +18,63 @@ window.onclick = function (event) {
 
 }
 
-//document.getElementById("search"). style.display = "none";
+document.getElementById("cryptoSearch"). style.display = "none";
+document.querySelector(".cryptoTable").style.display = "none";
+
+//Este codigo esconde el Search bar que va a buscar los cryptos en el modal. La idea es hacerlo aparecer cuando el botton de crypto sea seleccionado
+document.getElementById("cryptobtn").onclick = function () {
+    document.getElementById("stockbtn").style.display = "none"
+    document.getElementById("currencybtn").style.display = "none"
+    document.getElementById("cryptobtn").style.display = "none"
+    document.getElementById("cryptoSearch").style.display = "block"
+    document.querySelector(".cryptoTable").style.display = "block"
+}
+document.getElementById("")
 
 
 
+//Constantes establecidas
+const cryptoSearch = document.getElementById("cryptoSearch")
+const cryptoRows = document.getElementsByTagName("td")
 
+//Trae toda la informacion de cryptos
 let endpoint = "https://api.binance.com/api/v3/ticker/price";
 fetch(endpoint)
 .then( response =>response.json())
 .then( info => showInfo(info))
 .catch( error => console.log(error))
 
+//Enseña toda la informacion de cryptos en el modal como una tabla
 const showInfo = (info) => {
-    console.log(data)
-let body = ""
-for (let i = 0; i < data.length; i++) {
-    body += "<tr><td>${data[i].symbol}<td></tr> <tr><td>${data[i].price}<td></tr>"
-}
+    //console.log(info) AQUI TENGO QUE VER UNA MANERA DE FILTRAR PARA QUE HAGA ESTO UNICAMENTE PARA LAS QUE CONTIENEN USDT
+    let body = " "
+    for (let i = 0; i < 100; i++) {
+        body += `<tr><td>${info[i].symbol}<td><td>${info[i].price}<td></tr>`
+    }
 
 document.getElementById("data").innerHTML = body
 
 }
+
+//search
+cryptoSearch.addEventListener("keyup", (e)=>{
+    let text = e.target.value
+    //console.log(text)
+
+    let expression = new RegExp(text, "i")
+    for( let i = 0; i < cryptoRows.length; i++ ) {
+        let value = cryptoRows[i]
+        //console.log(value)
+        if(expression.test (value.innerText)){
+            value.classList.remove ("hide")
+
+        } 
+        
+        else {
+            console.log(value)
+            value.classList.add("hide")
+
+        }
+    }
+
+})
